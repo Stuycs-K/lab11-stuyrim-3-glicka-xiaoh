14080718c274712eec8f5bd104b94d4db6189bb9
hxiao61 <hxiao61@stuy.edu>
2025-01-09 09:15:06 -0600

    changed names need to change stats

--- CodeWarrior.java
+++ CodeWarrior.java
@@ -1,80 +1,79 @@
-public class CodeWarrior extends Adventurer{
-  int caffeine, caffeineMax;
-  String preferredLanguage;
+public class Warrior extends Adventurer{
+  int energy, energyMax;
+  String preferredClass;
 
   /*the other constructors ultimately call the constructor
   *with all parameters.*/
-  public CodeWarrior(String name, int hp, String language){
+  public Warrior(String name, int hp, String class){
     super(name,hp);
-    caffeineMax = 12;
-    caffeine = caffeineMax/2;
-    preferredLanguage = language;
+    energyMax = 12;
+    energy = energyMax/2;
+    preferredClass = class;
   }
 
-  public CodeWarrior(String name, int hp){
-    this(name,hp,"c++");
+  public Warrior(String name, int hp){
+    this(name,hp,"barbarian");
   }
 
-  public CodeWarrior(String name){
+  public Warrior(String name){
     this(name,24);
   }
 
-  public CodeWarrior(){
-    this("Carmack");
+  public Warrior(){
+    this("Griffith");
   }
 
   /*The next 8 methods are all required because they are abstract:*/
   public String getSpecialName(){
-    return "caffeine";
+    return "energy";
   }
 
   public int getSpecial(){
-    return caffeine;
+    return energy;
   }
 
   public void setSpecial(int n){
-    caffeine = n;
+    energy = n;
   }
 
   public int getSpecialMax(){
-    return caffeineMax;
+    return energyMax;
   }
 
-  /*Deal 2-7 damage to opponent, restores 2 caffeine*/
+  /*Deal 2-7 damage to opponent, restores 2 energy*/
   public String attack(Adventurer other){
     int damage = (int)(Math.random()*6)+2;
     other.applyDamage(damage);
     restoreSpecial(2);
     return this + " attacked "+ other + " and dealt "+ damage +
-    " points of damage. They then take a sip of their coffee.";
+    " points of damage. They then taunted.";
   }
 
-  /*Deal 3-12 damage to opponent, only if caffeine is high enough.
-  *Reduces caffeine by 8.
+  /*Deal 3-12 damage to opponent, only if energy is high enough.
+  *Reduces energy by 8.
   */
   public String specialAttack(Adventurer other){
     if(getSpecial() >= 8){
       setSpecial(getSpecial()-8);
       int damage = (int)(Math.random()*5+Math.random()*5)+3;
       other.applyDamage(damage);
-      return this + " used their "+preferredLanguage+
-      " skills to hack the matrix. "+
-      " This glitched out "+other+" dealing "+ damage +" points of damage.";
+      return this + " used their "+preferredClass+
+      " skills to damage"+other+" dealing "+ damage +" points of damage.";
     }else{
-      return "Not enough caffeine to use the ultimate code. Instead "+attack(other);
+      return "Not enough energy to use their ultimate. Instead "+attack(other);
     }
 
   }
   /*Restores 5 special to other*/
   public String support(Adventurer other){
-    return "Gives a coffee to "+other+" and restores "
+    return "Gives a warrior's shout to "+other+" and restores "
     + other.restoreSpecial(5)+" "+other.getSpecialName();
   }
   /*Restores 6 special and 1 hp to self.*/
   public String support(){
     int hp = 1;
     setHP(getHP()+hp);
-    return this+" drinks a coffee to restores "+restoreSpecial(6)+" "
+    return this+" pounds his chest to restores "+restoreSpecial(6)+" "
     + getSpecialName()+ " and "+hp+" HP";
   }
 }
