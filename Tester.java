0010fdbf5379c052d001e436d5c64666ab0a0aa6
Andrew <andrewg454@nycstudents.net>
2025-01-10 10:13:55 -0500

    printing but in infinite loop

---
+++ Tester.java
@@ -0,0 +1,36 @@
+public class Tester{
+  public static void main(String[] args){
+    Text.clear();
+    TextBox(1,1,1,1, "h");
+
+  }
+
+
+public static void drawText(String s,int startRow, int startCol){
+Text.go(startRow, startCol);
+System.out.println(Text.colorize(s, 37));
+  /*>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>*/
+  //YOUR CODE HERE
+  /*<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<*/
+}
+
+/*Use this method to place text on the screen at a particular location.
+*When the length of the text exceeds width, continue on the next line
+*for up to height lines.
+*All remaining locations in the text box should be written // Move to the next part of the text with spaces to
+*clear previously written text.
+*@param row the row to start the top left corner of the text box.
+*@param col the column to start the top left corner of the text box.
+*@param width the number of characters per row
+*@param height the number of rows
+*/
+public static void TextBox(int row, int col, int width, int height, String text){
+  for (int i = 0; i < height; i++) {
+    for(int w = 0; i < width/text.length(); w++){
+    drawText(text, row + i, col+w);
+  }
+}
+
+
+}
+}
