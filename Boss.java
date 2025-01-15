df33ebe12b6d4fefe80191e255e5aaa225c4b1e7
hxiao61 <hxia61@stuy.edu>
2025-01-14 19:03:33 -0500

    i think i finished it

--- Boss.java
+++ Boss.java
@@ -1,23 +1,23 @@
-public class Warrior extends Adventurer{
+public class Boss extends Adventurer{
   int mana, manaMax;
 
   /*the other constructors ultimately call the constructor
   *with all parameters.*/
-  public Warrior(String name, int hp){
+  public Boss(String name, int hp){
     super(name,hp);
     manaMax = 200;
     mana = 200;
   }
 
-  public Warrior(String name, int hp){
+  public Boss(String name, int hp){
     this(name,hp);
   }
 
-  public Warrior(String name){
+  public Boss(String name){
     this(name,100);
   }
 
-  public Warrior(){
+  public Boss(){
     this("Iron Fist");
   }
 
@@ -30,7 +30,7 @@ public class Warrior extends Adventurer{
   }
 
   public void setSpecial(int n){
-    mana = n;
+    mana = Math.min(n, manaMax);
   }
 
   public int getSpecialMax(){
@@ -38,27 +38,26 @@ public class Warrior extends Adventurer{
   }
 
   public String attack(Adventurer other){
-    int damage = 40; //lets do random from 30-50
+    int damage = (int) (Math.random() * 21) + 30; //lets do random from 30-50
     other.applyDamage(damage);
     restoreSpecial(5);
     return this + " used Jeet Kune Do on "+ other + " and dealt "+ damage +
-    " points of damage. They then meditated for .";
+    " points of damage. They then meditated for 5 mana";
   }
 
   public String specialAttack(Adventurer other){
     if(getSpecial() >= 20){
       setSpecial(getSpecial()-20);
-      int damage = 35; //change this to random from 30-50
+      int basedamage = (int)(Math.random() * 21) + 30;
+      int bonus = (int)(0.1 * other.getHP());
+      int damage = basedamage + bonus;
       other.applyDamage(damage);
-      return this + " used K'un-L "
-      " skills to damage"+other+" dealing "+ damage +" points of damage.";
+      return this + " used K'un-L kick to damage"+other+" dealing "+ damage +" points of damage.";
     }else{
-      return "Not enough mana to use their ultimate. Instead "+attack(other);
+      return "Not enough mana to use their special. Instead "+attack(other);
     }
-
   }
 
-//random 1 or 2, if 1 use harmony recovery, if 2 use dragons defense
   public String support(){
     int hp = 30;
     setHP(getHP()+hp);
